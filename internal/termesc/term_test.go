31d954e6fac1b9b663a907b6bf4166bd5fec4373
Diogo Pinela <diogoid7400@gmail.com>
2017-10-14 17:46:21 +0100

    Add test for standalone ESC handling

--- internal/termesc/term_test.go
+++ internal/termesc/term_test.go
@@ -1,10 +1,12 @@
 package termesc
 
 import (
+	"fmt"
 	"io"
 	"reflect"
 	"strings"
 	"testing"
+	"time"
 )
 
 const testInput = "\x1B[M#U7Á €50.0\x1B+25c, \x1B[Afoo\x1B[32;10;15M\x1B[Cπ"
@@ -14,9 +16,15 @@ var wantOutput = []string{"\x1B[M#U7", "Á", " ", "€", "5", "0", ".", "0", "\x
 
 func TestInputParsing(t *testing.T) {
 	c := NewConsoleReader(strings.NewReader(testInput))
-	var output []string
+	output := getAllOutput(t, c)
+	if !reflect.DeepEqual(output, wantOutput) {
+		t.Errorf("TestInputParsing: got %q, want %q", output, wantOutput)
+	}
+}
+
+func getAllOutput(t *testing.T, r *ConsoleReader) (output []string) {
 	for {
-		token, err := c.ReadToken()
+		token, err := r.ReadToken()
 		if err == io.EOF {
 			break
 		}
@@ -25,7 +33,23 @@ func TestInputParsing(t *testing.T) {
 		}
 		output = append(output, token)
 	}
-	if !reflect.DeepEqual(output, wantOutput) {
-		t.Errorf("TestInputParsing: got %q, want %q", output, wantOutput)
+	return
+}
+
+var wantOutput2 = []string{"\x1B", "["}
+
+func TestStandaloneEsc(t *testing.T) {
+	r, w := io.Pipe()
+	go func() {
+		// We don't check for the write errors here; any failure will be picked up by the read end
+		fmt.Fprint(w, "\x1B")
+		time.Sleep(50 * time.Millisecond)
+		fmt.Fprint(w, "[")
+		w.Close()
+	}()
+	c := NewConsoleReader(r)
+	output := getAllOutput(t, c)
+	if !reflect.DeepEqual(output, wantOutput2) {
+		t.Errorf("TestStandaloneEsc: got %q, want %q", output, wantOutput2)
 	}
 }
