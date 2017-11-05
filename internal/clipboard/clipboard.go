2a456707c9cceaf783f4a08f400a41d8e26d1b36
Diogo Pinela <diogoid7400@gmail.com>
2017-11-05 00:11:10 +0000

    Support system clipboard on macOS

--- internal/clipboard/clipboard.go
+++ internal/clipboard/clipboard.go
@@ -5,10 +5,13 @@
 package clipboard
 
 import (
+	"bytes"
 	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"runtime"
 
 	"github.com/dpinela/mflg/internal/atomicwrite"
 	"github.com/pkg/errors"
@@ -16,13 +19,14 @@ import (
 
 // Copy overwrites the clipboard's contents with the given data.
 func Copy(data []byte) error {
-	return copyGeneric(data)
+	return errors.Wrap(copyGeneric(data), "copy failed")
 }
 
 // Paste returns the last data stored with Copy by any instance of mflg of the same user,
 // or the last data copied into the system clipboard if that is supported.
 func Paste() ([]byte, error) {
-	return pasteGeneric()
+	data, err := pasteGeneric()
+	return data, errors.Wrap(err, "paste failed")
 }
 
 func homeDirectory() (string, error) {
@@ -52,25 +56,48 @@ func mkMflgPath(elems ...string) (string, error) {
 	return p, os.MkdirAll(p, 0700)
 }
 
-const genericClipboardFile = "clipboard"
-
 func copyGeneric(data []byte) error {
-	const errString = "copy failed"
-
-	p, err := mkMflgPath()
-	if err != nil {
-		return errors.Wrap(err, errString)
+	if runtime.GOOS == "darwin" {
+		if err := copyToPasteboard(data); err == nil {
+			return nil
+		}
 	}
-	err = atomicwrite.Write(filepath.Join(p, genericClipboardFile), func(w io.Writer) error { _, err := w.Write(data); return err })
-	return errors.Wrap(err, errString)
+	return copyBuiltin(data)
 }
 
 func pasteGeneric() ([]byte, error) {
-	const errString = "paste failed"
+	if runtime.GOOS == "darwin" {
+		if data, err := pastePasteboard(); err == nil {
+			return data, nil
+		}
+	}
+	return pasteBuiltin()
+}
+
+const genericClipboardFile = "clipboard"
+
+func copyBuiltin(data []byte) error {
+	p, err := mkMflgPath()
+	if err != nil {
+		return err
+	}
+	return atomicwrite.Write(filepath.Join(p, genericClipboardFile), func(w io.Writer) error { _, err := w.Write(data); return err })
+}
+
+func pasteBuiltin() ([]byte, error) {
 	p, err := mflgPath(genericClipboardFile)
 	if err != nil {
-		return nil, errors.Wrap(err, errString)
+		return nil, err
 	}
-	data, err := ioutil.ReadFile(p)
-	return data, errors.Wrap(err, errString)
+	return ioutil.ReadFile(p)
+}
+
+func copyToPasteboard(b []byte) error {
+	copyCmd := exec.Command("pbcopy")
+	copyCmd.Stdin = bytes.NewReader(b)
+	return copyCmd.Run()
+}
+
+func pastePasteboard() ([]byte, error) {
+	return exec.Command("pbpaste").Output()
 }
