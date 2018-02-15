94069893a8890417b708b0d3038ac7688b0c0368
Diogo Pinela <diogoid7400@gmail.com>
2018-02-15 23:54:38 +0000

    Preserve permissions of original file when using atomicwrite.Write

--- internal/atomicwrite/write.go
+++ internal/atomicwrite/write.go
@@ -9,9 +9,12 @@ import (
 	"github.com/pkg/errors"
 )
 
+const defaultPerms = os.FileMode(0640)
+
 // Write atomically overwrites the file at filename with the content written by the
 // given function.
-// The file is created if it doesn't already exist.
+// The file is created with mode 0640 if it doesn't already exist; if it does, its permissions will be
+// preserved if possible.
 func Write(filename string, contentWriter func(io.Writer) error) error {
 	tf, err := ioutil.TempFile("", "mflg-atomic-write")
 	if err != nil {
@@ -23,6 +26,13 @@ func Write(filename string, contentWriter func(io.Writer) error) error {
 		tf.Close()
 		return errors.Wrap(err, errString(filename))
 	}
+	// Keep existing file's permissions, when possible. This may race with a chmod() on the file.
+	perms := defaultPerms
+	if info, err := os.Stat(filename); err == nil {
+		perms = info.Mode()
+	}
+	// It's better to save a file with the default TempFile permissions than not save at all, so if this fails we just carry on.
+	tf.Chmod(perms)
 	if err = tf.Close(); err != nil {
 		os.Remove(name)
 		return errors.Wrap(err, errString(filename))
