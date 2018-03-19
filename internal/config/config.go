3f22e666f934f605e4dbd137b24077a671517fa4
Diogo Pinela <diogoid7400@gmail.com>
2018-03-19 20:03:10 +0000

    Change expected location of configuration file to mflg/config.toml
    This puts mflg more in line with other things that get their config
    from the XDG directories, such as fish.

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,7 @@ import (
 	"github.com/BurntSushi/toml"
 	"github.com/pkg/errors"
 	"github.com/tajtiattila/basedir"
+	"path/filepath"
 )
 
 type Config struct {
@@ -23,9 +24,10 @@ func (c *Config) ConfigForExt(ext string) *LangConfig { return c.Lang[ext] }
 // Load finds and reads the primary configuration file for the current user, according to the
 // XDG base directory specification for configuration files. It always returns a usable *Config,
 // even if it also returns a non-nil error.
+// The file is expected to be at mflg/config.toml in one of the appropriate configuration directories.
 func Load() (*Config, error) {
 	c := Config{TabWidth: 4, Lang: make(map[string]*LangConfig)}
-	f, err := basedir.Config.Open("mflg.toml")
+	f, err := basedir.Config.Open(filepath.Join("mflg", "config.toml"))
 	if err != nil {
 		return &c, errors.WithMessage(err, "error loading config file")
 	}
